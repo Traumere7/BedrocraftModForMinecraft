bef91a086994103b78928d97c633c0aac12c2f27
Traumere7 <3253974127@qq.com>
2026-04-06 20:40:29 +0800

    Initial commit

---
+++ src/main/java/com/traumere/bedrocraft/BedrocraftClient.java
@@ -0,0 +1,8 @@
+package com.traumere.bedrocraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BedrocraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
---
+++ src/main/java/com/traumere/bedrocraft/BedrocraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.traumere.bedrocraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BedrocraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
